7ff9f80c38f40fbc3c909e6b63ef8ed29605eb25
abdmhamzah <iamhamzaham@gmail.com>
2021-06-28 11:47:00 +0700

    added username and password field with login button

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -3,6 +3,43 @@ import 'package:flutter/material.dart';
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _usernameField(),
+            _passwordField(),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _usernameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.person),
+        hintText: "Username",
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        icon: Icon(Icons.security),
+        hintText: "Password",
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text("Login"),
+    );
   }
 }
